7cc9f7a033fc988aab488f1dfa20c67d9ecb37da
apeXchimpz / AXC <149444039+apeXchimpz-AXC@users.noreply.github.com>
2023-11-01 14:23:21 +0100

    Update hellowold.sol

--- hellowold.sol
+++ hellowold.sol
@@ -7,7 +7,7 @@ Project overview:
 ApeXChimpz or in short “AXC” is a project founded in 6/2021 in the Polkadot eco system. 
 We create equippable NFTs (NFT 2.0) based on the RMRK lego technology and can already look back at a rich history and a great community backing our efforts.
 Utilizing the newest tech and putting a unique twist on NFTs. 
-We developed the first marketing agency on ASTAR Network where use equippable and customizable NFTs (XCHimpz) to connect advertisers, projects, communities and the NFT holders themselves to form upsides to all parties involved.
+We developed the first marketing agency on ASTAR Network where we use equippable and customizable NFTs (XCHimpz) to connect advertisers, projects, communities and the NFT holders themselves to form upsides to all parties involved.
 
 We are part of the builders program, check out our proposal here:
 https://forum.astar.network/t/unstoppable-grants-proposal-apexchimpz-axc-full-ip/5256
